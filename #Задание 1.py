e3491a917ffec3d7d4e16ed0add39544346ab862
maehwa <68327208+maehwaaa@users.noreply.github.com>
2025-08-30 16:15:30 +0600

    Create #Задание 1.py

---
+++ #Задание 1.py
@@ -0,0 +1,13 @@
+#Задание 1
+class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Autobus(Transport):
+    pass
+
+bus = Autobus("Renaul Logan", 180, 12)
+
+print(f"Название авто: {bus.name} Скорость: {bus.max_speed} Пробег: {bus.mileage}")
